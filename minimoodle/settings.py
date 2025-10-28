26619882435744d4aaccd8ef3920b0b6406f186d
Muhd Fahad <your_email@example.com>
2025-10-28 11:56:02 +0000

    latest

--- minimoodle/settings.py
+++ minimoodle/settings.py
@@ -59,12 +59,13 @@ TEMPLATES = [
         'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,                   
         'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-            'courses.context_processors.categories',  # <--- added
+            'debug':True,
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'courses.context_processors.categories',  # <--- added
         ],
     },
     },
